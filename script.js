1c436b1985c6acf71795678ba16ccbed7b6272b2
h0dkv <hristian.26v@oupvolov.com>
2025-03-18 21:44:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const leaders = [
-    { name: "Winston Churchill", country: "United Kingdom", img: "https://media.gettyimages.com/id/3432929/photo/prime-minister-of-great-britain-winston-churchill-makes-his-ve-day-broadcast-to-the-world.jpg?s=612x612&w=0&k=20&c=4Wmkd4fxTBCwv17FAa2RO0c0ztemY7BoFJAqZK9M8I8=", hp: 100, resources: 50, abilities: [{name: "Inspire", effect: "heal", value: 20, cost: 10, cooldown: 3, currentCooldown: 0, voiceLine: "We shall never surrender."}] },
-    { name: "Franklin D. Roosevelt", country: "United States", img: "https://media.gettyimages.com/id/3252562/photo/franklin-delano-roosevelt-the-32nd-president-of-the-united-states-from-1933-45-a-democrat-he-led.jpg?s=612x612&w=0&k=20&c=eemysMqyxdzX1Y4d0ktUlyAltVl9gFaXNp9j99FUSGQ=", hp: 100, resources: 50, abilities: [{name: "New Deal", effect: "heal", value: 15, cost: 10, cooldown: 2, currentCooldown: 0, voiceLine: "The only thing we have to fear is fear itself."}] },
-    { name: "Joseph Stalin", country: "Soviet Union", img: "https://media.gettyimages.com/id/527191260/photo/portrait-of-joseph-stalin.jpg?s=612x612&w=0&k=20&c=GI9AdVXamVUKYU2O772iDdkxp0vOmWiQa0gg2LwLtMY=", hp: 100, resources: 50, abilities: [{name: "Iron Fist", effect: "damage", value: 25, cost: 15, cooldown: 4, currentCooldown: 0, voiceLine: "The death of one man is a tragedy, the death of millions is a statistic."}] },
-    { name: "Adolf Hitler", country: "Germany", img: "https://media.gettyimages.com/id/119505258/photo/adolf-hitler-in-munich-in-the-spring-of-1932.jpg?s=612x612&w=0&k=20&c=SPyMava8n_tHW4Dm5ygM1GNYZwjC-gv4tEejHV_1GrQ=", hp: 100, resources: 50, abilities: [{name: "Blitzkrieg", effect: "damage", value: 30, cost: 20, cooldown: 5, currentCooldown: 0, voiceLine: "I will make Germany great again."}] },
-    { name: "Benito Mussolini", country: "Italy", img: "https://media.gettyimages.com/id/107708027/photo/italy-a-portrait-of-the-duce-benito-mussolini-between-1937-and-1940-portrait-of-the-duce.jpg?s=612x612&w=0&k=20&c=oIlr_n25KE3Oi36S8xTxc3adcdpvNlBahI-5GNY3qL4=", hp: 100, resources: 50, abilities: [{name: "Propaganda", effect: "heal", value: 10, cost: 5, cooldown: 1, currentCooldown: 0, voiceLine: "It is better to live one day as a lion than 100 years as a sheep."}] },
-    { name: "Hideki Tojo", country: "Japan", img: "https://media.gettyimages.com/id/515361304/photo/former-japanese-premier-hideki-tojo-shot-himself-on-september-11-inflicting-a-serious-wound.jpg?s=612x612&w=0&k=20&c=4MHlZ6BhSHVCVD4mQzvCqEexhOCYE2QHYR1SQ18G0qo=", hp: 100, resources: 50, abilities: [{name: "Kamikaze", effect: "damage", value: 35, cost: 25, cooldown: 6, currentCooldown: 0, voiceLine: "Duty is heavier than a mountain, death is lighter than a feather."}] }
-    { name: "Sophie Rain", country: "Mitko", img: "https://s.yimg.com/ny/api/res/1.2/6uyAnva6kOr7zg4ct59d8g--/YXBwaWQ9aGlnaGxhbmRlcjt3PTE2Mzg7aD0yMDQ4/https://media.zenfs.com/en/e__181/47fbf98152ea1a4ea51cae5dfb28f385", hp:100, resources:50, abilities:[{name:Sigma Suck, effect:"damage", value: 100, cost:15, cooldown: 0, currentCooldown: 0, voiceLine: "I suck the balls of every rapper" }];
+    { name: "Winston Churchill", country: "United Kingdom", img: "https://media.gettyimages.com/id/3432929/photo/prime-minister-of-great-britain-winston-churchill-makes-his-ve-day-broadcast-to-the-world.jpg?s=612x612&w=0&k=20&c=4Wmkd4fxTBCwv17FAa2RO0c0ztemY7BoFJAqZK9M8I8=", hp: 100, resources: 50, abilities: [{ name: "Inspire", effect: "heal", value: 20, cost: 10, cooldown: 3, currentCooldown: 0, voiceLine: "We shall never surrender." }] },
+    { name: "Franklin D. Roosevelt", country: "United States", img: "https://media.gettyimages.com/id/3252562/photo/franklin-delano-roosevelt-the-32nd-president-of-the-united-states-from-1933-45-a-democrat-he-led.jpg?s=612x612&w=0&k=20&c=eemysMqyxdzX1Y4d0ktUlyAltVl9gFaXNp9j99FUSGQ=", hp: 100, resources: 50, abilities: [{ name: "New Deal", effect: "heal", value: 15, cost: 10, cooldown: 2, currentCooldown: 0, voiceLine: "The only thing we have to fear is fear itself." }] },
+    { name: "Joseph Stalin", country: "Soviet Union", img: "https://media.gettyimages.com/id/527191260/photo/portrait-of-joseph-stalin.jpg?s=612x612&w=0&k=20&c=GI9AdVXamVUKYU2O772iDdkxp0vOmWiQa0gg2LwLtMY=", hp: 100, resources: 50, abilities: [{ name: "Iron Fist", effect: "damage", value: 25, cost: 15, cooldown: 4, currentCooldown: 0, voiceLine: "The death of one man is a tragedy, the death of millions is a statistic." }] },
+    { name: "Adolf Hitler", country: "Germany", img: "https://media.gettyimages.com/id/119505258/photo/adolf-hitler-in-munich-in-the-spring-of-1932.jpg?s=612x612&w=0&k=20&c=SPyMava8n_tHW4Dm5ygM1GNYZwjC-gv4tEejHV_1GrQ=", hp: 100, resources: 50, abilities: [{ name: "Blitzkrieg", effect: "damage", value: 30, cost: 20, cooldown: 5, currentCooldown: 0, voiceLine: "I will make Germany great again." }] },
+    { name: "Benito Mussolini", country: "Italy", img: "https://media.gettyimages.com/id/107708027/photo/italy-a-portrait-of-the-duce-benito-mussolini-between-1937-and-1940-portrait-of-the-duce.jpg?s=612x612&w=0&k=20&c=oIlr_n25KE3Oi36S8xTxc3adcdpvNlBahI-5GNY3qL4=", hp: 100, resources: 50, abilities: [{ name: "Propaganda", effect: "heal", value: 10, cost: 5, cooldown: 1, currentCooldown: 0, voiceLine: "It is better to live one day as a lion than 100 years as a sheep." }] },
+    { name: "Hideki Tojo", country: "Japan", img: "https://media.gettyimages.com/id/515361304/photo/former-japanese-premier-hideki-tojo-shot-himself-on-september-11-inflicting-a-serious-wound.jpg?s=612x612&w=0&k=20&c=4MHlZ6BhSHVCVD4mQzvCqEexhOCYE2QHYR1SQ18G0qo=", hp: 100, resources: 50, abilities: [{ name: "Kamikaze", effect: "damage", value: 35, cost: 25, cooldown: 6, currentCooldown: 0, voiceLine: "Duty is heavier than a mountain, death is lighter than a feather." }] },
+    { name: "Sophie Rain", country: "Mitko", img: "https://s.yimg.com/ny/api/res/1.2/6uyAnva6kOr7zg4ct59d8g--/YXBwaWQ9aGlnaGxhbmRlcjt3PTE2Mzg7aD0yMDQ4/https://media.zenfs.com/en/e__181/47fbf98152ea1a4ea51cae5dfb28f385", hp: 100, resources: 50, abilities: [{ name: "Sigma Suck", effect: "damage", value: 100, cost: 15, cooldown: 0, currentCooldown: 0, voiceLine: "I suck the balls of every rapper." }] }
+];
 
 const choices = ["Attack", "Defend", "Use Ability"];
 const leaderSelection = document.getElementById("leaders");
@@ -49,7 +50,7 @@ function selectLeader(playerIndex) {
     playerLeader = leaders[playerIndex];
     enemyLeader = leaders[Math.floor(Math.random() * leaders.length)];
 
-    playVoiceLine(playerLeader);
+    playVoiceLine(playerLeader.abilities[0].voiceLine);
 
     document.getElementById("leader-selection").style.display = "none";
     pickedLeaders.style.display = "block";
@@ -191,9 +192,13 @@ function playSoundEffect(effect) {
     sound.play();
 }
 
-function playVoiceLine(leader) {
-    const voiceLine = new SpeechSynthesisUtterance(leader.abilities[0].voiceLine);
-    speechSynthesis.speak(voiceLine);
+function playVoiceLine(text) {
+    if ('speechSynthesis' in window) {
+        const utterance = new SpeechSynthesisUtterance(text);
+        speechSynthesis.speak(utterance);
+    } else {
+        console.error('Speech synthesis not supported in this browser.');
+    }
 }
 
 // Restart game
